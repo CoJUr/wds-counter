87dcfa43a12804070f8fb3491a44da014317d53e
“Cory <coryurton@gmail.com>
2022-11-21 00:01:48 -0600

    feat: setting state w/o prevState, bad

--- src/CounterHooks.js
+++ src/CounterHooks.js
@@ -5,9 +5,9 @@ export default function CounterHooks( { initialCount } ) {
     const [state, setState] = useState({ count: initialCount }); {/* destructuring the returned 2-value array from useState */}
     return (
         <div>
-            <button >-</button>
+            <button onClick={() => setState({count: state.count-1 })}>-</button>
             <span> {state.count} </span>
-            <button  >+</button>
+            <button onClick={() => setState({count: state.count+1 })} >+</button>
         </div>
         )
 }
