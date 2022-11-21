1a374e925cabecfe4a270c840a7caa49faca366d
“Cory <coryurton@gmail.com>
2022-11-20 23:01:02 -0600

    incrementing with vs without prevState

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import React from "react";
-
+import Counter from "./Counter";
 
 function App() {
-  return "Hello world"
+  return (
+    <Counter initialCount={0} />
+  )
 }
 
 export default App;
