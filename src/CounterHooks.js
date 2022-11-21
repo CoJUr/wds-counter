2d47113dbde8883911a498c0903fcc44ca857027
“Cory <coryurton@gmail.com>
2022-11-21 00:17:09 -0600

    feat: correction, now its a number - no brackets

--- src/CounterHooks.js
+++ src/CounterHooks.js
@@ -2,7 +2,7 @@ import React,  { useState } from 'react'
 
 export default function CounterHooks( { initialCount } ) {
     // eslint-disable-next-line no-lone-blocks
-    const [count, setCount] = useState({ initialCount }); {/* destructuring the returned 2-value array from useState */}
+    const [count, setCount] = useState(initialCount); {/* destructuring the returned 2-value array from useState */}
     return (
         <div>
             <button onClick={() => setCount(prevCount => prevCount -1)}>-</button> 
