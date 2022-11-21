b98e2f4a8a492a40972077b0fd31ea360a62c9dd
“Cory <coryurton@gmail.com>
2022-11-21 00:08:00 -0600

    using prevState in setState, good

--- src/CounterHooks.js
+++ src/CounterHooks.js
@@ -5,7 +5,9 @@ export default function CounterHooks( { initialCount } ) {
     const [state, setState] = useState({ count: initialCount }); {/* destructuring the returned 2-value array from useState */}
     return (
         <div>
-            <button onClick={() => setState({count: state.count-1 })}>-</button>
+            <button onClick={() => setState(prevState => {
+                return { count: state.count-1 }
+                })}>-</button> 
             <span> {state.count} </span>
             <button onClick={() => setState({count: state.count+1 })} >+</button>
         </div>
