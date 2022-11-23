c16bc98b36ff3e50b124063f782a511bea188344
“Cory <coryurton@gmail.com>
2022-11-22 22:41:50 -0600

    intro to createContext, toggling theme in state

--- src/App.js
+++ src/App.js
@@ -1,18 +1,30 @@
-import React from "react";
+import React, { useState } from "react";
 import Counter from "./Counter";
 import CounterHooks from "./CounterHooks";
 
+export const ThemeContext = React.createContext() // 2 parts  1 provider to pass vals, 1 consumer which consumes vals
+//must use state to rerender app in order to change context
 
 
 function App() {
 
+
+  const [theme, setTheme] = useState('red')
+
   return (
-    <>
+    <ThemeContext.Provider value={{ backgroundColor: theme }} >
+    {/* counter and counter hooks can now access provider */}
       Counter
       <Counter initialCount={0} />
       Counter Hooks
       <CounterHooks initialCount={3} />
-    </>
+      {/* create a button to toggle theme */}
+      <button onClick={() => setTheme(prevTheme => {
+        return prevTheme === 'red' ? 'blue':'red' 
+        }) 
+        }> Toggle Theme </button>
+      
+    </ThemeContext.Provider>
   )
 }
 
