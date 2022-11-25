f248502546d06bcdf435109d1d94211ed373a6bf
“Cory <coryurton@gmail.com>
2022-11-24 22:37:44 -0600

    passing down recipes to RecipeList correctly but with unique key missing error

--- src/RecipeList.js
+++ src/RecipeList.js
@@ -2,11 +2,14 @@
 import React from 'react'
 import Recipe from './Recipe'
 
-export default function RecipeList() {
+export default function RecipeList({ recipes }) {
   return (
-    <>
-    <Recipe />
-    <Recipe />
-    </>
+    <div>
+
+   {recipes.map(recipe => {
+    {/* <Recipe id={recipes.id} name={recipes.name} ></Recipe>   instead: => spread operator */}
+    return<Recipe {...recipe} /> // props lets pass down all properties as top levels of prop rather than being nested in the recipe
+   })}
+    </div>
   )
 }
