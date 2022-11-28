5125a0ce57cd3d8b1a632b00a613ed1b09aefaa2
“Cory <coryurton@gmail.com>
2022-11-27 21:22:00 -0600

    feat: css for left side of page

--- src/components/Recipe.js
+++ src/components/Recipe.js
@@ -12,29 +12,29 @@ export default function Recipe( props ) {
     ingredients
     } = props
   return (
-    <div>
-        <div>
-            <h3>{name}</h3>
+    <div className='recipe'>
+        <div className='recipe__header' >
+            <h3 className='recipe__title'>{name}</h3>
             <div>
-                <button>Edit</button>
-                <button>Delete</button>
+                <button className='btn btn--primary mr-1'>Edit</button>
+                <button className='btn btn--danger'>Delete</button>
             </div>
         </div>
-        <div>
-            <span>Cook Time:</span>
-            <span>{ cookTime }</span>
+        <div className='recipe__row'>
+            <span className='recipe__label'>Cook Time:</span>
+            <span className='recipe__value'>{ cookTime }</span>
         </div>
-        <div>
-            <span>Servings:</span>
-            <span> {servings} </span>
+        <div className='recipe__row'>
+            <spa className='recipe__label'n>Servings:</spa>
+            <span className='recipe__value'> {servings} </span>
         </div>
-        <div>
-            <span>Instructions:</span>
-            <div>{instructions}</div>
+        <div className='recipe__row'>
+            <span className='recipe__labe recipe__instructions recipe__label--indented'>Instructions:</span>
+            <div className='recipe__value recipe__instructions recipe__value--indented'>{instructions}</div>
         </div>
-        <div>
-            <span>Ingredients:</span>
-            <div>
+        <div className='recipe__row'>
+            <span className='recipe__label'>Ingredients:</span>
+            <div className='recipe__value recipe__value--indented'>
                 <IngredientList 
                 //get and render ingredients from here in Recipe
                 ingredients={ ingredients } />
